cac14da1fa366e65aabfa9c30f848c6920bd15f2
Edward Shanahan <312564962+edwardshanahan97@users.noreply.github.com>
2026-09-21 13:29:48 +0100

    feat: add note filtering

--- index.js
+++ index.js
@@ -5,37 +5,49 @@ const notes = [
     id: 1,
     title: "Note 1",
     description: "This is a note 1",
+    tag: "personal",
   },
 
   {
     id: 2,
     title: "Note 2",
     description: "This is a note 2",
+    tag: "work",
   },
 
   {
     id: 3,
     title: "Note 3",
     description: "This is a note 3",
+    tag: "personal",
   },
 
   {
     id: 4,
     title: "Note 4",
     description: "This is a note 4",
+    tag: "work",
   },
 
   {
     id: 5,
     title: "Note 5",
     description: "This is a note 5",
+    tag: "personal",
   },
 ];
 
 const app = express();
 
 app.get("/notes", (req, res) => {
-  res.json(notes);
+  let results = notes;
+  const tag = req.query.tag;
+
+  if (tag) {
+    results = notes.filter((note) => note.tag === tag);
+  }
+
+  res.json(results);
 });
 
 app.get("/notes/:id", (req, res) => {
